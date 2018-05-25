b8bf0b2a2e9c757e885a4fbc04bd9226bfbdba5f
arcobaleno <670014298@qq.com>
2018-05-25 19:03:47 +0800

    czy

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-window.onload=function(){
-    var active = document.getElementById("active");
-    var currentX;
-    var div = document.getElementById("frame");
-    var li = div.children[0].children[0].children[0];
-    active.style.left=li.offsetLeft+"px";
-    var beginX = li.offsetLeft;
-    var lis = div.children[0].children;
-    for(var i=0;i<lis.length;i++){
-        if(i%2 !=0){
-            continue;
-        }
-        lis[i].onmouseover = function(event){
-            event = window.event||event;
-            var src = event.srcElement;
-            currentX = src.offsetLeft;
-            setInterval(function(){
-                beginX= beginX+(currentX-beginX)/10;
-                active.style.left = beginX+"px";
-            },25);
-        }
-    }
-}
+function addListener(element, type, callback) {
+		    if (element.addEventListener) {
+		        element.addEventListener(type, callback);
+		    } else if (element.attachEvent) {
+		        element.attachEvent('on' + type, callback);
+		    }
+		}
+
+		addListener(document, 'DOMContentLoaded', function () {
+		    
+		    var mq = window.matchMedia("(max-width: 760px)");
+		    if (mq.matches) {
+		        document.getElementById("menu_list").classList.add("hidden");
+		    }
+		    
+		    addListener(window, 'resize', function () {
+		        var width = window.innerWidth ||
+		                    document.documentElement.clientWidth ||
+		                    document.body.clientWidth;
+		        
+		        if (width > 760) {
+		            document.getElementById("menu_list").classList.remove("hidden");
+		        } else {
+		            document.getElementById("menu_list").classList.add("hidden");
+		        }
+		    });
+		    
+		});
